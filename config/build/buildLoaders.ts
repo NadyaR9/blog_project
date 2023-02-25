1193157df78166d1d897411cd579cbc6e91541e7
Razinkova Nadezhda <nrazinkova@roox.ru>
2023-02-25 12:11:34 +0300

    fix: fix theme import

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { RuleSetRule } from 'webpack';
 import { BuildOptions } from './types/config';
-import { buildCssLoader } from './loaders/buildCssLoader';
 import { buildSvgLoader } from './loaders/buildSvgLoader';
 
 export default function buildLoaders({ isDev, paths }: BuildOptions): RuleSetRule[] {
@@ -23,7 +23,24 @@ export default function buildLoaders({ isDev, paths }: BuildOptions): RuleSetRul
       },
     },
   };
-  const stylesLoader = buildCssLoader(isDev);
+  const stylesLoader = {
+    test: /\.scss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash:base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
