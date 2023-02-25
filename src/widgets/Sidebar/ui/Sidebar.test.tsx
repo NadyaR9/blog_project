5c3c979cb38c5d8f4e7889dd077fe94abf0293b5
Razinkova Nadezhda <nrazinkova@roox.ru>
2023-02-25 22:26:09 +0300

    update loki screenshots, add reports

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/config/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/config/tests';
 import { Sidebar } from './Sidebar';
 
 describe('test Sidebar', () => {
   test('test Sidebar render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar'));
   });
   test('test Sidebar collapsed render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleButton = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleButton);
